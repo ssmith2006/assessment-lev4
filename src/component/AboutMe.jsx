bf8f6da7bb536b798af1b1aefed38b24f3f2832f
ssmith2006 <shantel.smith@hotmail.com>
2025-07-31 22:55:54 +0000

    added styling, completed project

--- src/component/AboutMe.jsx
+++ src/component/AboutMe.jsx
@@ -1,22 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "react-bootstrap/Card";
 
 export default function AboutMe() {
+  const hobbies = ["Baking", "Singing", "Crafting"];
+  const [showFunFact, setShowFunFact] = useState(false);
   return (
     <>
-      <h1>ABOUT ME!!!!</h1>
-      <section>
-        <Card
-          className="d-flex justify-content-center"
-          style={{ width: "18rem" }}
-        >
+      <h1 className="font-header">ABOUT ME!!!!</h1>
+      <section className="d-flex justify-content-center gap-3 mt-5 flex-wrap">
+        <Card style={{ width: "18rem" }}>
           {/* need to figure out how to get card to center.  Drawing a blank.  Come back after coding is done for styling */}
           <Card.Img
             variant="top"
             src="/me.jpg"
             alt="Shantel"
-            style={{ height: "300px", objectFit: "cover" }}
+            style={{ height: "175px", objectFit: "cover" }}
           />
           <Card.Body>
             <Card.Title>Shantel Smith</Card.Title>
@@ -29,7 +28,7 @@ export default function AboutMe() {
         </Card>
       </section>
 
-      <section>
+      <section className="section-learning mt-5">
         <h2>Learning Style</h2>
         <p>
           There are many ways I prefer to learn. No, there isn't one correct way
@@ -43,35 +42,37 @@ export default function AboutMe() {
           lectures. It helps me visualize what is being taught because I can see
           it right in front of me.
         </p>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img
-            variant="top"
-            src="/hands-on.png"
-            alt="Hands On"
-            style={{ height: "300px", objectFit: "cover" }}
-          />
-        </Card>
+        <div className="d-flex justify-content-center gap-3 flex-wrap">
+          <Card style={{ width: "18rem" }}>
+            <Card.Img
+              variant="top"
+              src="/hands-on.png"
+              alt="Hands On"
+              style={{ height: "150px", objectFit: "cover" }}
+            />
+          </Card>
 
-        <Card style={{ width: "18rem" }}>
-          <Card.Img
-            variant="top"
-            src="/youtube.jpg"
-            alt="YouTube logo"
-            style={{ height: "300px", objectFit: "cover" }}
-          />
-        </Card>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img
+              variant="top"
+              src="/youtube.jpg"
+              alt="YouTube logo"
+              style={{ height: "150px", objectFit: "cover" }}
+            />
+          </Card>
 
-        <Card style={{ width: "18rem" }}>
-          <Card.Img
-            variant="top"
-            src="/handwritten.jpg"
-            alt="Handwritten Notes"
-            style={{ height: "300px", objectFit: "cover" }}
-          />
-        </Card>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img
+              variant="top"
+              src="/handwritten.jpg"
+              alt="Handwritten Notes"
+              style={{ height: "150px", objectFit: "cover" }}
+            />
+          </Card>
+        </div>
       </section>
 
-      <section>
+      <section className="mt-5">
         <h2>"What I Loved From My Previous Level"</h2>
         <p>
           To be honest, I wasn't too thrilled with Level 3. I knew it was going
@@ -79,8 +80,64 @@ export default function AboutMe() {
           the asynchronous testing, Webpack, useEffect/useState, amongst many
           others. Level 3 wasn't all bad though! Learning AWS Databases was
           something I enjoyed. Building tables and being able to create in
-          DynamoDB was awesome!
+          DynamoDB was awesome! My former instructor introduced us to SASS which
+          was very good!
         </p>
+      </section>
+
+      <section className=" section-concepts mt-5" >
+        <h2>"Where I'm At"</h2>
+        <p>
+          <ul>
+            <li>
+              <p>
+                There are a few concepts that I feel comfortable working. I
+                wouldn't say confident. Throughout Level 3, I grabbed the
+                concept of working in the terminal. At first I didn't like it
+                but eventually came to like it. I feel comfortable using
+                DynamoDB for APIs as well as using Nelify. Browser Routes was
+                pretty cool. Async functions are ok.
+              </p>
+            </li>
+            <li>
+              <p>
+                Whew, where do I start? There are many concepts that I just
+                don't get or can't retain enough information to even know the
+                reason behind it. I can complete many concepts like
+                useState/useEffect, for-loops, map, slice, join, and many
+                others.
+              </p>
+            </li>
+            <li>
+              <p>
+                As I stated before, I'm very curious about learning more AWS
+                DynamoDB. Any other web services in AWS have caught my
+                attention.
+              </p>
+            </li>
+          </ul>
+        </p>
+      </section>
+      <section className="mt-5">
+        <h3>Shantel's Favorite Hobbies</h3>
+        <ul>
+          {hobbies.map((hobby, index) => (
+            <li key={index}>{hobby}</li>
+          ))}
+        </ul>
+      </section>
+      <section className="section-fun mt-5">
+        <h2>Shantel's Fun Fact</h2>
+        <button onClick={() => setShowFunFact(!showFunFact)}>
+          {showFunFact ? "Hide Fun Fact" : "Shantel's Fun Fact"}
+        </button>
+
+        {showFunFact && (
+          <p>
+            Fun Fact: I am a certified Crisis Counselor that assist with
+            Disaster Recovery.
+          </p>
+        )}
       </section>
     </>
   );
---
+++ uniqueCharacters.js
@@ -0,0 +1,12 @@
+//uniqueCharacters.js
+/*
+Pseudocode:
+1. Convert the string to characters
+2. Loop through each character
+3. Use a Set to track seen characters
+4. If a duplicate is found, return false
+5. If all characters are unique, return true
+*/
+
+
+//I don't remember Unique Characters from Level 3.  Looking through my notes, I don't see any mention.  I'll have to check my dashboard from Level 3.
