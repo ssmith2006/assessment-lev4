193c296ae32eff796b71eb316f09f88fc6a5684b
ssmith2006 <shantel.smith@hotmail.com>
2025-08-01 01:26:46 +0000

    updated unique file

--- uniqueCharacters.js
+++ uniqueCharacters.js
@@ -10,3 +10,24 @@ Pseudocode:
 
 
 //I don't remember Unique Characters from Level 3.  Looking through my notes, I don't see any mention.  I'll have to check my dashboard from Level 3.
+
+
+function hasUniqueChars(str){
+    const seenChars=[]
+    for (let i= 0; i<str.length; i++){
+        const currentChar=str[i]
+
+        if(seenChars.includes(currentChar)){
+            return false;
+        }
+        seenChars.push(currentChar);
+    }
+    return true;
+}
+
+console.log(hasUniqueChars("goodbye")); //false
+console.log(hasUniqueChars("Shantel")); //true
+console.log(hasUniqueChars("Prscilla")); //false
+console.log(hasUniqueChars("Gabriel")); //true
+console.log(hasUniqueChars("Turtle")); //false--I wanted to create an error
+console.log(hasUniqueChars("turtle")); //false
